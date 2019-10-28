e9a7a30b604c7bc30c815c62b459e766c35f7789
tom <kotom320@gmail.com>
2019-10-28 10:53:51 +0900

    test

--- src/main/java/com/prj/enjoy/HomeController.java
+++ src/main/java/com/prj/enjoy/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	//test for git
+	//change nothing
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
