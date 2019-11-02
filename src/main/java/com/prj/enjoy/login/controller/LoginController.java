975b207eeb2ce4513e9ed2ca9ce67a296b05e949
DONGWOOK <kotom320@gamil.com>
2019-11-02 15:00:14 +0900

    logindao에 searchvo 제거

--- src/main/java/com/prj/enjoy/login/controller/LoginController.java
+++ src/main/java/com/prj/enjoy/login/controller/LoginController.java
@@ -1,9 +1,7 @@
 package com.prj.enjoy.login.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,7 +10,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
