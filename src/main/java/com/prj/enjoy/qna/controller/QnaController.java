82b1557208da847f9c92fbfd7962995da27c511f
DONGWOOK <kotom320@gmail.com>
2019-11-07 11:06:01 +0900

    sbcontent수정
    -삭제 ,수정버튼 글쓴이와 로그인한 사람이 같을 경우만 보이게 변경
    -form 형식을multipart로 변경
    -sql 문 수정

--- src/main/java/com/prj/enjoy/qna/controller/QnaController.java
+++ src/main/java/com/prj/enjoy/qna/controller/QnaController.java
@@ -9,12 +9,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.JsonViewResponseBodyAdvice;
 
 import com.prj.enjoy.qna.dao.QnaDao;
 import com.prj.enjoy.qna.dto.QnaDto;
