250915abd7e311067b1406cd777bdb5df0b8befc
DONGWOOK <kotom320@gmail.com>
2019-11-20 15:29:31 +0900

    sblist 더보기 갯수 수정
    6 >>> 4

--- src/main/java/com/prj/enjoy/saleboard/controller/SbController.java
+++ src/main/java/com/prj/enjoy/saleboard/controller/SbController.java
@@ -72,7 +72,7 @@ public class SbController {
 
 		SbDao dao = sqlSession.getMapper(SbDao.class);
 		int sNum = Integer.parseInt(request.getParameter("num")) + 1;
-		int eNum = Integer.parseInt(request.getParameter("num")) + 6;
+		int eNum = Integer.parseInt(request.getParameter("num")) + 4;
 
 		ArrayList<SbDto> addDto = dao.add_list2(sNum, eNum);
 
@@ -85,7 +85,7 @@ public class SbController {
 
 		SbDao dao = sqlSession.getMapper(SbDao.class);
 		int sNum = Integer.parseInt(request.getParameter("num")) + 1;
-		int eNum = Integer.parseInt(request.getParameter("num")) + 6;
+		int eNum = Integer.parseInt(request.getParameter("num")) + 4;
 
 		ArrayList<SbDto> addDto = dao.add_list3(sNum, eNum);
 
